1234a110c2f517111c822a087b26f87821da5506
saaid323 <69105920+saaid323@users.noreply.github.com>
2024-03-28 23:30:02 +0300

    corrected a typo

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 CKEDITOR_UPLOAD_PATH = "uploads/"
